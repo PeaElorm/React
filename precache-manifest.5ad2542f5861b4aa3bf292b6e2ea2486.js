d183b7db5c03f7969db9c913b0c66a155c74a579
unknown <elormavugal7@gmail.com>
2020-08-12 11:36:08 +0000

    Updates

---
+++ precache-manifest.5ad2542f5861b4aa3bf292b6e2ea2486.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d467a33bbbc679d7b9c425e4f0bad8b",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "d1157e3d0526ec6aeb95",
+    "url": "/react/static/css/main.0c983815.chunk.css"
+  },
+  {
+    "revision": "88ce5a3c4afe4b5f1f7d",
+    "url": "/react/static/js/2.89697ab7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.89697ab7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1157e3d0526ec6aeb95",
+    "url": "/react/static/js/main.1fb8d71f.chunk.js"
+  },
+  {
+    "revision": "e13f72c0a5b2117a7fa1",
+    "url": "/react/static/js/runtime-main.da8bee19.js"
+  },
+  {
+    "revision": "ca2ff46308bf756b57a965975e146995",
+    "url": "/react/static/media/anastasiya-pavlova-IcLqFMC3h08-unsplash.ca2ff463.jpg"
+  },
+  {
+    "revision": "dcafbcac12cfee62d872c1857cde03ff",
+    "url": "/react/static/media/anastasiya-pavlova-SW9sp2asIX8-unsplash.dcafbcac.jpg"
+  },
+  {
+    "revision": "a3798f03acc280aac643cb692927fd3e",
+    "url": "/react/static/media/andreas-dress-IxiCibeZC10-unsplash.a3798f03.jpg"
+  }
+]);
